7532a208dc47bc4753865a9b1494f946e52d2962
arafat-ali <anarafat13@gmail.com>
2023-09-17 02:46:07 +0600

    Category view and add complete

--- models/operation.php
+++ models/operation.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once './input.php';
+require_once 'input.php';
 
 class Operation extends Input{
     public $option;
     private $options = [
-        "1" => "Add Category",
-        "2" => "View Categories",
+        "1" => "View Categories",
+        "2" => "Add Category",
         "3" => "Add Income",
         "4" => "Add Expense",
         "5" => "View Income List",
@@ -14,17 +14,8 @@ class Operation extends Input{
         "0" => "Exit Application"
     ];
 
-
-    private function showOptions(){
-        echo "\n";
-        foreach($this->options as $id => $option){
-            echo "\t Press $id to - $option \n";
-        }
-    }
-
-    public function start(){
-        echo "\n\t Wellcome to Income-Expense Tracker System!";
-        $this->showOptions();
+    public function showOperationalOptions():void{
+        $this->showOption($this->options);
     }
 
 
